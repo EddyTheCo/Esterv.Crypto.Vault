5bd320e4c0767ca896782d852388499210ed36d0
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-06-29 11:55:52 +0200

    added the option to change password from the cli #4

--- src/QVaultCli.cpp
+++ src/QVaultCli.cpp
@@ -9,11 +9,14 @@ int main(int argc, char *argv[])
     QCoreApplication::setApplicationName("VaultCli");
 
     QCommandLineParser parser;
-    parser.setApplicationDescription("Get and set data from the Vault file. Only one mode can be set [Set|Get].");
+    parser.setApplicationDescription("Get/Set data from/to the Vault file. Only one mode can be set [Set|Get].");
     parser.addHelpOption();
-    parser.addPositionalArgument("data",
-                                 QCoreApplication::translate("main", "data to store on the vault. Only on Set Mode."));
     parser.addPositionalArgument("pass", QCoreApplication::translate("main", "password of the vault."));
+    parser.addPositionalArgument("newpass",
+                                 QCoreApplication::translate("main", "new password of the vault. Only with -sc mode."));
+    parser.addPositionalArgument("data",
+                                 QCoreApplication::translate("main", "data to store on the vault. Only with -s mode"));
+
     QString fileHelp = "[optional] file path to the vault.\n default:" +
                        (QStandardPaths::writableLocation(QStandardPaths::AppLocalDataLocation) + "/qvault.bin");
     parser.addPositionalArgument("file", QCoreApplication::translate("main", fileHelp.toLocal8Bit().constData()));
@@ -26,11 +29,16 @@ int main(int argc, char *argv[])
                                           << "set",
                             QCoreApplication::translate("main", "set the data of the vault => Set Mode."));
     parser.addOption(setO);
+    QCommandLineOption setP(QStringList() << "c"
+                                          << "cpass",
+                            QCoreApplication::translate("main", "change vault password => Set Mode."));
+    parser.addOption(setP);
 
     parser.process(app);
     const QStringList args = parser.positionalArguments();
     bool getMode = parser.isSet(getO);
     bool setMode = parser.isSet(setO);
+    bool changePassword = parser.isSet(setP);
 
     if ((setMode && getMode) || (!setMode && !getMode))
         parser.showHelp();
@@ -59,7 +67,14 @@ int main(int argc, char *argv[])
     }
     else
     {
-        vault->setData(args.at(0).toUtf8(), args.at(1).toUtf8());
+        if (changePassword)
+        {
+            vault->changePassword(args.at(0).toUtf8(), args.at(1).toUtf8());
+        }
+        else
+        {
+            vault->setData(args.at(0).toUtf8(), args.at(1).toUtf8());
+        }
     }
 
     return 0;
